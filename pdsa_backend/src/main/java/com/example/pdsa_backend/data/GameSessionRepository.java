aa0a6374c7010541559f7599e17edf7914349761
Liviru-Navaratna <livirunavaratna@gmail.com>
2025-04-19 13:06:58 +0530

    adjusted backend data files

--- pdsa_backend/src/main/java/com/example/pdsa_backend/data/GameSessionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pdsa_backend.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameSessionRepository extends JpaRepository<GameSession, Integer> {
-}
-
