ce4b8c90db74648aaba0dc2d6ec870c3d410dd89
Liviru-Navaratna <livirunavaratna@gmail.com>
2025-04-27 23:50:53 +0530

    repo changes

--- pdsa_backend/src/main/java/com/example/pdsa_backend/data/travelingsalesmandata/PerformanceMetricRepository.java
+++ pdsa_backend/src/main/java/com/example/pdsa_backend/data/travelingsalesmandata/PerformanceMetricRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PerformanceMetricRepository extends JpaRepository<PerformanceMetric, Integer> {
+    Optional<PerformanceMetric> findByResultIdAndAlgorithmId(int resultId, int algorithmId);
     List<PerformanceMetric> findByResultId(int resultId);
 }
