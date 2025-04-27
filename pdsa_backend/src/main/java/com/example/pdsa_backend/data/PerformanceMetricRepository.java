8230290b41d0949597eba3c9dc954f296ae78dc5
Liviru-Navaratna <124782159+Liviru-Nava@users.noreply.github.com>
2025-04-28 00:45:09 +0530

    Update PerformanceMetricRepository.java

--- pdsa_backend/src/main/java/com/example/pdsa_backend/data/PerformanceMetricRepository.java
+++ pdsa_backend/src/main/java/com/example/pdsa_backend/data/PerformanceMetricRepository.java
@@ -1,7 +1,6 @@
-package com.example.pdsa_backend.data.travelingsalesmandata;
+package com.example.pdsa_backend.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
